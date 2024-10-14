e0e04ebfa33301807725dd8c69fd750abc03145f
sabina-shakiyeva <121431790+sabina-shakiyeva@users.noreply.github.com>
2024-10-15 00:18:58 +0400

    Add files via upload

---
+++ Server/Client/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+
+var port = 5000;
+var ipAddress = IPAddress.Parse("127.0.0.1");
+var ep = new IPEndPoint(ipAddress, port);
+var client = new TcpClient();
+
+try
+{
+    client.Connect(ep);
+    if (client.Connected)
+    {
+        var networkStream = client.GetStream();
+
+        while (true)
+        {
+
+            Console.Write("Taking screenshot. Write 'exit' to stop or press Enter to continue:");
+
+            string input = Console.ReadLine();
+            if (input == "exit")
+            {
+                networkStream.Close();  
+                client.Close(); 
+                break;
+                break;
+            }
+
+
+            string path = "screenshot";
+            using (Bitmap bitmap = new Bitmap(1920, 1080))
+            {
+                using Graphics g = Graphics.FromImage(bitmap);
+                g.CopyFromScreen(Point.Empty, Point.Empty, new Size(1920, 1080));
+                bitmap.Save($"{path}.png", ImageFormat.Png);
+            }
+
+
+            var screenshotPath = $"{path}.png";
+            using (var readFs = new FileStream(screenshotPath, FileMode.Open, FileAccess.Read))
+            {
+                int len = 0;
+                var buffer = new byte[1024];
+                while ((len = readFs.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    networkStream.Write(buffer, 0, len);
+                }
+            }
+
+            Console.WriteLine("Screenshot sent to server.");
+
+
+            Thread.Sleep(10000);
+        }
+
+
+        networkStream.Close();
+        client.Close();
+        Console.WriteLine("Disconnected from server.");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+
+var port = 5000;
+var ipAddress = IPAddress.Parse("127.0.0.1");
+var ep = new IPEndPoint(ipAddress, port);
+
+using var listener = new TcpListener(ep);
+try
+{
+    listener.Start();
+    Console.WriteLine("Server started...");
+
+    while (true)
+    {
+        var client = listener.AcceptTcpClient();
+        _ = Task.Run(() =>
+        {
+            Console.WriteLine($"{client.Client.RemoteEndPoint} connected");
+            var stream = client.GetStream();
+
+
+            int count = 1;
+            try
+            {
+                while (true)
+                {
+
+                    string screenshotPath = $@"C:\Users\shaki\source\repos\Server\Server\ScreenPhotos\screenshot_{count++}.png";
+
+                    using (var fs = new FileStream(screenshotPath, FileMode.Create, FileAccess.Write))
+                    {
+                        int len;
+                        var buffer = new byte[1024];
+                        while ((len = stream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            fs.Write(buffer, 0, len);
+
+
+                            if (len < buffer.Length)
+                            {
+                                break;
+                            }
+                          
+                        }
+                        if (len == 0)
+                        {
+                            Console.WriteLine("Client disconnected.");
+                            break;
+                        }
+                    }
+
+                    Console.WriteLine($"Screenshot saved at {screenshotPath}");
+                    //try
+                    //{
+                    //    byte[] testBuffer = new byte[1];
+                    //    stream.Write(testBuffer, 0, testBuffer.Length);
+                    //}
+                    //catch (IOException)
+                    //{
+                    //    Console.WriteLine("Client disconnected (detected via stream.Write).");
+                    //    break;
+                    //}
+
+
+                    if (!client.Connected)
+                    {
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                client.Close();
+                Console.WriteLine($"{client.Client.RemoteEndPoint} disconnected");
+            }
+        });
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
